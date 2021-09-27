fca5ed05ecba2a37b8b4534e4fa44903da0b5b7b
Matias Barrientos <matias.barrientosg@gmail.com>
2021-09-27 17:31:27 -0300

    Fix default bayesian config

--- src/bayesian_tuning.rs
+++ src/bayesian_tuning.rs
@@ -21,10 +21,10 @@ pub struct BayesianConfig {
 
 impl BayesianConfig {
     pub fn new(scenario: &Scenario) -> Self {
-        let population_size = 2;//10 * scenario.parameters().nb_params();
+        let population_size = 10 * scenario.parameters().nb_params();
         Self {
             population_size,
-            max_iterations:1,//5 * scenario.parameters().nb_params(),
+            max_iterations: 5 * scenario.parameters().nb_params(),
             select_size: (0.3 * population_size as f64).ceil() as usize,
             nb_children: population_size / 2,
         }
